050e528bd946ffe8a384f085662acec821b17960
lucascarl011 <lucassscarlosss54@gmail.com>
2024-08-16 18:09:33 -0300

    Estilizando a home

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,10 @@
 import { NavBar } from "../navBar/NavBar";
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.png'
 
 export function Header () {
     return (
-        <header className="max-w-7xl flex justify-between px-4 mx-auto">
-            <img className="w-44" src={logo}  alt="logo" />
+        <header className="max-w-7xl flex items-center justify-between p-4">
+            <img className="w-11" src={logo}  alt="logo" />
             <NavBar />
         </header>
     )
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,28 @@
-import { ListarCategorias } from "../categorias/listarCategoria/ListarCategoria";
+import farmacia from '../../assets/farmacia.jpg'
+
 
 export function Home() {
-    return (
-        <>
-        <h1>Home</h1>
-        <ListarCategorias />
-        </>
-    )
+  return (
+    <main className="max-w-7xl mx-auto space-y-8 mt-10">
+      <h1 className="text-[#485462] text-3xl font-bold text-center">Farma Gold</h1>
+
+
+      <div className="flex items-center gap-4">
+        <img className='size-[300px] object-cover' src={farmacia} alt="farmácia" />
+        <div className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-4'>
+            <strong className='text-center text-xl'>Serviços</strong>
+            <p className='text-xl px-12'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem modi molestias, laboriosam, cumque debitis earum, ipsa temporibus totam eligendi nulla repellendus? Voluptatibus, doloribus quas corrupti possimus fuga corporis assumenda temporibus.</p>
+          </div>
+          <div className='flex flex-col gap-4'>
+            <strong className='text-center text-xl'>Produtos</strong>
+            <p className='text-xl px-12'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem modi molestias, laboriosam, cumque debitis earum, ipsa temporibus totam eligendi nulla repellendus? Voluptatibus, doloribus quas corrupti possimus fuga corporis assumenda temporibus.</p>
+          </div>
+        
+        </div>
+
+
+      </div>
+    </main >
+  )
 }
